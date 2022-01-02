6971b51e04231dc368bff447be39ab5319c4c8b2
cvvsu <65501675+cvvsu@users.noreply.github.com>
2022-01-02 17:37:37 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-# -*- coding: utf-8 -*-
-
-
 from model import NPFDetection
-
 from options import get_args
 
 
@@ -15,20 +11,20 @@ if __name__=='__main__':
     #############################
     # draw NPF images
     #############################
-    #NPF.draw_one_day_images()
-    #NPF.draw_two_day_images()
+    NPF.draw_one_day_images()
+    NPF.draw_two_day_images()
 
 
     #############################
     # Get masks for NPF images
     #############################
-    # NPF.detect_one_day_masks()
-    # NPF.detect_two_day_masks()
+    NPF.detect_one_day_masks()
+    NPF.detect_two_day_masks()
 
     #############################
     # Visualize and select masks
     #############################
-    # NPF.visualize_masks()
+    NPF.visualize_masks()
 
     #####################################
     # Save the start-end times and GRs
